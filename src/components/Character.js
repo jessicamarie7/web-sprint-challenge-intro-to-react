7cebe2ff417c925e26ad2f6f350148ae2f3bf110
Jessica Manak <jessica8angels@gmail.com>
2021-10-08 10:03:57 -0700

    keep getting Cannot read properties of null (reading map) so Im giving up

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,33 @@
-// Write your Character component here
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../constants";
+
+
+
+export default function Character() {
+
+    const [character, setCharacter] = useState(null)
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}`)
+        .then(res => {
+            setCharacter(res.data)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    }, [])
+
+
+    const characterList = character.map(person => {
+        return (person.name)
+    })
+
+
+
+    return (
+        <div>
+            <h2>Testing Characters {characterList}</h2>
+        </div>
+    )
+}
